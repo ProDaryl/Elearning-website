a888d414a13de240a8b4224e70048c7b6c6134e7
ProDaryl <ejoheshingwe.daryl@ictuniversity.edu.cm>
2024-08-11 04:43:49 +0000

    started he django app

---
+++ registration/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+# Create your views here.
+def register(request): 
+    pass
