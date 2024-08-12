17b554fc63c31ba055d8e269e1c08f638fc50dd7
ProDaryl <ejoheshingwe.daryl@ictuniversity.edu.cm>
2024-08-12 00:40:01 +0000

    connected the project

--- registration/urls.py
+++ registration/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register, name='register'),
-    path('login', views.login, name='login')
-}
+    path('index', views.index, name='index'),
+    path('signup', views.register, name='signup'),
+    path('login', views.login, name='login'),
+    path('dashboard', views.dashboard, name='dashboard')
+]
--- registration/views.py
+++ registration/views.py
@@ -8,7 +8,27 @@ def index(request):
     return render(request, 'index.html')
 
 def register(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST('username')
+        email = request.POST('email')
+        password = request.POST('password')
+        confirm_pass = request.POST('confirmPass')
+
+        if password == confirm_pass:
+            if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+                messages.info(request, 'User already exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                return redirect('dashboard')
+        else:
+            messages.info(request, 'Passwords mismatch')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
+            
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
 def login(request):
-    pass
+    return render(request, 'login.html')
