c9aa53b773451227d2a7f763cbf4a0bb37ab436f
ProDaryl <ejoheshingwe.daryl@ictuniversity.edu.cm>
2024-08-18 21:52:13 +0100

    Checkpoint

--- registration/urls.py
+++ registration/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('signup', views.register, name='signup'),
     path('login', views.login, name='login'),
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('select_role', views.select_role, name='select_role'),
+    path('forms', views.role_form, name='forms')
 ]
