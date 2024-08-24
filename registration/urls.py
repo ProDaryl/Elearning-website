9003fa73514b8c6964c04199e81a9953616098d9
ProDaryl <ejoheshingwe.daryl@ictuniversity.edu.cm>
2024-08-24 17:24:27 +0100

    Minor Changes

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 from django.urls import include, path
-=======
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
->>>>>>> 1631a8e1a0468c37b92b59c1a6f3b4158cb451ba
 from . import views
 # from django.contrib.auth import views as auth_views
 
@@ -15,16 +12,6 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('signup', views.register, name='signup'),
     path('login', views.login, name='login'),
-<<<<<<< HEAD
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('course/', views.course, name='course'), 
-    path('teacher/', views.teacher, name='teacher'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('select_role', views.select_role, name='select_role'),
-    path('forms', views.role_form, name='forms')
-]
-=======
     # path('dashboard', views.dashboard, name='dashboard'),
     path('forms', views.role_form, name='forms'),
     path('logout', views.log_out, name='logout'),
@@ -34,4 +21,3 @@ urlpatterns = [
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 1631a8e1a0468c37b92b59c1a6f3b4158cb451ba
